cd327261f0c818b2c16c90ffd21d7a5eb06443e8
kelly wolosonovich <k.wolosonovich@gmail.com>
2020-10-02 16:09:08 -0700

    added routes & models folders, implemented get routes

---
+++ routes/companies.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const ExpressError = require("../helpers/ExpressError")
+const Company = require('../models/Company')
+
+const router = new express.Router();
+
+router.get("/", async function(req, res, next) {
+  try {
+    let result = await Company.getAll();
+    return res.json({ result });
+  } catch (err) {
+    return next(err);
+  }
+});
