3c7268b28c5ad4d78f1d64793135b843f0885b75
kelly wolosonovich <k.wolosonovich@gmail.com>
2020-10-07 09:57:05 -0700

    added job and users tables

--- data.sql
+++ data.sql
@@ -5,3 +5,22 @@ CREATE TABLE companies (
     description TEXT, 
     logo_url TEXT
 );
+
+CREATE TABLE jobs (
+    id INTEGER PRIMARY KEY, 
+    title TEXT NOT NULL, 
+    salary MONEY NOT NULL,
+    equity FLOAT CHECK(equity <= 1.0),
+    company_handle TEXT NOT NULL REFERENCES companies ON DELETE CASCADE
+
+);
+
+CREATE TABLE users(
+    username TEXT PRIMARY KEY,
+    password TEXT NOT NULL,
+    first_name TEXT,
+    last_name TEXT,
+    email TEXT,
+    photo_url TEXT,
+    is_admin BOOLEAN NOT NULL default FALSE
+);
