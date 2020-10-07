69058d955658446a782adbeede6417a67f4060c9
kelly wolosonovich <k.wolosonovich@gmail.com>
2020-10-07 13:00:06 -0700

    added test framework

---
+++ __tests__/integration/companies.test.js
@@ -0,0 +1,17 @@
+const request = require('supertest')
+const app = require("../../app");
+
+
+beforeEach(async function () {
+  await beforeEachHook(TEST_DATA);
+});
+
+// tests 
+
+afterEach(async function () {
+  await afterEachHook();
+});
+
+afterAll(async function () {
+  await afterAllHook();
+});
