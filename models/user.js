5d974eaec3cbfaba90456c88bf60d8869d1977eb
kelly wolosonovich <k.wolosonovich@gmail.com>
2020-10-08 11:07:23 -0700

    implemented users routes

--- models/user.js
+++ models/user.js
@@ -1,8 +1,79 @@
 const db = require("../db");
 const ExpressError = require("../helpers/expressError");
+const sqlForPartialUpdate = require("../helpers/partialUpdate");
+
 
 class User {
 
+    static getAll = async() => {
+        const result = await db.query(
+          `SELECT username, first_name, last_name 
+            FROM users`
+        );
+        return result.rows
+    } 
+
+    static getUser = async(username) => {
+      const result = await db.query(
+        `SELECT username, first_name, last_name, email, photo_url
+            FROM users
+            WHERE username = $1`,
+            [username]
+      )
+      const user = result.rows[0];
+      if (!user) {
+        throw new ExpressError("Match not found", 404);
+      }
+      return user;
+    }
+
+    static add = async(data) => {
+        const result = await db.query(
+          `INSERT INTO users 
+                (username, password, first_name, last_name, email, photo_url)
+                VALUES ($1, $2, $3, $4, $5, $6)
+                RETURNING username, first_name, last_name, email, photo_url`,
+            [
+                data.username,
+                data.password, 
+                data.first_name,
+                data.last_name,
+                data.email,
+                data.photo_url, 
+          ]
+        );
+        return result.rows[0]
+    }
+
+    static update = async(username, data) => {
+        const { query, values } = sqlForPartialUpdate(
+          "users",
+          data,
+          "username",
+          username
+        );
+
+        const result = await db.query(query, values)
+        const user = result.rows[0]
+
+        if (!user) {
+            throw new ExpressError("Company not found", 404);
+        }
+        return user
+    }
+
+    static delete = async(username) => {
+        const result = await db.query(
+            `DELETE FROM users
+            WHERE username = $1
+            RETURNING username`,
+            [username]
+        )
+        if (result.rows.length === 0) {
+          throw new ExpressError("User not found", 404);
+        }
+        return result.rows[0];
+    }
 }
 
 module.exports = User;
--- routes/users.js
+++ routes/users.js
@@ -9,13 +9,66 @@ const User = require("../models/user");
 const router = express.Router();
 
 
-// get user 
+// GET - all users with filter
 
-// add user 
+router.get("/", async (req, res, next) => {
+  try {
+    const result = await User.getAll(req.query);
+    return res.json({ result });
+  } catch (err) {
+    return next(err);
+  }
+});
 
-// update user
+// GET - search username 
 
-// delete user 
+router.get("/:username", async (req, res, next) => {
+  const username = req.params.username;
+  try {
+    const result = await User.getUser(username);
+    return res.status(201).json({ user: result });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+// POST - add new user 
+
+router.post("/", async(req, res, next) => {
+  let user = req.body 
+  try {
+    const result = await User.add(user)
+    return res.json({ user: result})
+  } catch (err) {
+    return next(err);
+  }
+})
+
+// PATCH - update user
+
+router.patch("/:username", async(req, res, next) => {
+  let username = req.params.username
+  let data = req.body 
+  try {
+    const result = await User.update(username, data);
+    return res.json({ updated: result });
+  } catch (err) {
+    return next(err);
+  }
+})
+
+
+// DELETE - delete user 
+
+router.delete("/:username", async(req, res, next) => {
+  let username = req.params.username
+  try {
+    const result = await User.delete(username)
+    return res.json({ message: 'User deleted' });
+  } catch (err) {
+    return next(err)
+  }
+})
 
 
 module.exports = router;
