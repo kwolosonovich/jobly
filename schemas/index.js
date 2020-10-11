b889067d322470a4615d437490f34ede3fc1092a
kelly wolosonovich <k.wolosonovich@gmail.com>
2020-10-10 21:10:34 -0700

    updated job schema

--- schemas/index.js
+++
@@ -1,7 +0,0 @@
-exports.companySchema = require("./companySchema");
-exports.updateCompanySchema = require("./updateCompanySchema");
-exports.jobSchema = require("./jobSchema");
-exports.updateJobSchema = require("./updatejobSchema");
-exports.userAuthSchema = require("./userAuthSchema");
-exports.userSchema = require("./userSchema");
-exports.updateUserSchema = require("./updateUserSchema");
