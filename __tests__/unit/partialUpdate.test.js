6c01f19dd50a688059d9016026ff5efebbe3c9b8
kelly wolosonovich <k.wolosonovich@gmail.com>
2020-10-10 21:18:42 -0700

    updated integration tests files; added unit test

--- __tests__/unit/partialUpdate.test.js
+++ __tests__/unit/partialUpdate.test.js
@@ -1,9 +1,18 @@
-describe("partialUpdate()", () => {
-  it("should generate a proper partial update query with just 1 field",
-      function () {
+const sqlForPartialUpdate = require("../../helpers/partialUpdate");
 
-    // FIXME: write real tests!
-    expect(false).toEqual(true);
+describe("partialUpdate", () => {
+  it("should generate proper partial update query with 1 field", function () {
+    const { query, values } = sqlForPartialUpdate(
+      "jobs",
+      { title: "New job title" },
+      "id",
+      1
+    );
 
+    expect(query).toEqual(
+      "UPDATE jobs SET title=$1 WHERE id=$2 RETURNING *"
+    );
+
+    expect(values).toEqual(["New job title", 1]);
   });
 });
