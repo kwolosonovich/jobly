96cb9866393a78e5dc9c4c5db0ee1af14996606f
kelly wolosonovich <k.wolosonovich@gmail.com>
2020-10-17 19:05:39 -0700

    troubleshooting testing

--- __tests__/integration/jest.config.js
+++ __tests__/integration/jest.config.js
@@ -1,73 +1,78 @@
-process.env.NODE_ENV = "test";
+// process.env.NODE_ENV = "test";
 
-const request = require("supertest");
-const jwt = require("jsonwebtoken");
-const bcrypt = require("bcrypt");
+// const request = require("supertest");
+// const jwt = require("jsonwebtoken");
+// const bcrypt = require("bcrypt");
 
-const app = require("../../app");
-const db = require("../../db");
+// const app = require("../../app");
+// const db = require("../../db");
 
-const Company = require("../../models/company");
-const Job = require("../../models/job");
-const User = require("../../models/user");
+// const Company = require("../../models/company");
+// const Job = require("../../models/job");
+// const User = require("../../models/user");
+// const ExpressError = require("../../helpers/expressError");
 
-const TEST_DATA = {};
+// let AUTH = {
+//   token: undefined,
+//   username: undefined,
+// };
 
-beforeEachHook = async(TEST_DATA) => {
-    await db.query('DELETE FROM companies');
-    await db.query("DELETE FROM jobs");
-    await db.query("DELETE FROM users");
+// beforeEachHook = async(AUTH) => {
+//     await db.query('DELETE FROM companies');
+//     await db.query("DELETE FROM jobs");
+//     await db.query("DELETE FROM users");
 
-    try {
-      const hashedPassword = await bcrypt.hash("secret", 1);
+//     try {
+//       const hashedPassword = await bcrypt.hash("secret", 1);
 
-      let testUser = await db.query(
-        `INSERT INTO users (username, password, first_name, last_name, email, is_admin)
-                    VALUES ('testUser', $1, 'testFrist', 'testLast', 'test@email.com', true) RETURNING username, is_admin`,
-        [hashedPassword]
-      );
+//       await db.query(
+//         `INSERT INTO users (username, password, first_name, last_name, email, is_admin)
+//                     VALUES ('testUser', $1, 'testFrist', 'testLast', 'test@email.com', true) RETURNING username, is_admin`,
+//         [hashedPassword]
+//       );
 
-      const response = await request(app).post("/login").send({
-        username: "testUser",
-        password: "password",
-      });
+//       const response = await request(app).post("/login").send({
+//         username: "testUser",
+//         password: "password",
+//       });
 
-      TEST_DATA.userToken = response.body.token;
-      TEST_DATA.currentUsername = jwt.decode(TEST_DATA.userToken).username;
+//       AUTH.token = response.body.token;
+//       AUTH.username = jwt.decode(AUTH.token).username;
 
-    } catch (error) {
-      return (error);
-    }
+//     } catch (error) {
+//       return (error);
+//     }
 
-    await Company.add({
-      handle: "c1",
-      name: "Company1",
-      num_employees: 10,
-      description: "Company1 description",
-      logo_url: "company1Url",
-    });
-    await Job.add({
-        id: 999,
-        title: "job1", 
-        salary: 1000, 
-        equity: 0.5,
-        company_handle: "c1"
-    })
-}
+//     await Company.add({
+//       handle: "c1",
+//       name: "Company1",
+//       num_employees: 10,
+//       description: "Company1 description",
+//       logo_url: "company1Url",
+//     });
+//     await Job.add({
+//         id: 999,
+//         title: "job1", 
+//         salary: 1000, 
+//         equity: 0.5,
+//         company_handle: "c1"
+//     })
+// }
     
-afterEachHook = async () => {
-    await db.query("DELETE FROM companies");
-    await db.query("DELETE FROM jobs");
-    await db.query("DELETE FROM users");
-};
+// afterEachHook = async () => {
+//     await db.query("DELETE FROM companies");
+//     await db.query("DELETE FROM jobs");
+//     await db.query("DELETE FROM users");
+// };
 
-afterAllHook = async() => {
-    await db.end();
-}
+// afterAllHook = async() => {
+//     await db.end();
+// }
 
-module.exports = {
-  beforeEachHook,
-  afterEachHook,
-  afterAllHook,
-  verbose: true
-};
+// module.exports = {
+//   beforeEachHook,
+//   afterEachHook,
+//   afterAllHook,
+//   AUTH,
+//   verbose: true,
+// };
--- __tests__/integration/users.test.js
+++ __tests__/integration/users.test.js
@@ -1,74 +1,171 @@
 process.env.NODE_ENV = "test";
 
+// const request = require("supertest");
+// const app = require("../../app");
+// const ExpressError = require("../../helpers/expressError");
+
+// const getToken = require("../../helpers/token");
+
+// const {
+//   beforeEachHook,
+//   afterAllHook,
+//   AUTH,
+//   afterEachHook
+// } = require("./jest.config");
+
+// beforeEach(async function () {
+//   await beforeEachHook(AUTH);
+// });
+
+
 const request = require("supertest");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+
 const app = require("../../app");
-const getToken = require("../../helpers/token");
+// const db = "jobly-test";
+const db = require("../../db");
 
-const {
-  beforeEachHook,
-  afterAllHook,
-  TEST_DATA
-} = require("./jest.config");
 
-beforeEach(async function () {
-  await beforeEachHook(TEST_DATA);
-});
+const User = require("../../models/user");
+const ExpressError = require("../../helpers/expressError");
+
+// const { Client } = require("pg");
+// const client = new Client("jobly-test");
+
+
+// client.connect();
+
+
+
+let AUTH = {
+  token: undefined,
+  username: undefined,
+};
+
+beforeEach = async () => {
+  // await db.query("DELETE FROM companies");
+  // await db.query("DELETE FROM jobs");
+  await db.query("DELETE FROM users");
+
+  try {
+    const hashedPassword = await bcrypt.hash("secret", 1);
+
+    await db.query(
+      `INSERT INTO users (username, password, first_name, last_name, email, is_admin)
+                    VALUES ('testUser', $1, 'testFrist', 'testLast', 'test@email.com', true) RETURNING username, is_admin`,
+      [hashedPassword]
+    );
+
+    const received= await request(app).post("/login").send({
+      username: "testUser",
+      password: "password",
+    });
+
+    AUTH.token = result.body.token;
+    AUTH.username = jwt.decode(AUTH.token).username;
+
+  } catch (error) {
+    return error;
+  }
+};
+
 
 describe("GET, /users", () => {
   test("get users", async () => {
-    const result = await request(app).get("/users");
-    expect(result.statusCode).toBe(200);
-    expect(result.body.users[0]).toHaveProperty("username");
+    const received= await request(app).get('/users');
+    console.log(result.body.users)
+    expect(received.statusCode).toBe(200);
+    expect(received.body.users[0]).toHaveProperty("username");
   });
 });
 
 describe("GET, /users/:username", () => {
   test("get username", async () => {
-    const result = await request(app).get("/users/testUser");
-    expect(result.statusCode).toBe(201);
-    expect(result.body.user.username).toEqual("testUser");
+    const received= await request(app).get("/users/testUser");
+    expect(received.statusCode).toBe(201);
+    console.log(result.body)
+    expect(received.body.user.username).toEqual("testUser");
   });
 });
 
-describe("POST, /users", () => {
-  test("register user", async () => {
-    const result = await request(app).post("/users").send({
-      username: "testUser2",
-      password: "testPassword2",
-      first_name: "testFirstName2",
-      last_name: "testLastName2",
-      email: "testEmail2",
-      photo_url: "testURL2",
-    });
+// describe("POST, /users", () => {
+//   test("register user", async () => {
+//     const received = await request(app).post("/users").send({
+//       username: "testUser2",
+//       password: "testPassword2",
+//       first_name: "testFirstName2",
+//       last_name: "testLastName2",
+//       email: "testEmail2",
+//       photo_url: "testURL2",
+//     });
 
-    expect(result.statusCode).toBe(200);
-    expect(result.body).toHaveProperty("token");
-  });
-});
+//     expect(received.statusCode).toBe(200);
+//     expect(received.body).toHaveProperty("token");
+//   });
+// });
 
-describe("PATCH /users/username", () => {
-  test("update user data", async () => {
-    const result = await request(app).patch("/users/testUser").send({
-      first_name: "testFirstName2",
-      last_name: "testLastName2",
-      email: "updated",
-      photo_url: "testURL2",
-      token: TEST_DATA.userToken,
-    });
+// describe("PATCH /users/username", () => {
+//   test("update user data", async () => {
+//     const received= await request(app).patch("/users/testUser").send({
+//       first_name: "testFirstName2",
+//       last_name: "testLastName2",
+//       email: "updated",
+//       photo_url: "testURL2",
+//       token: DATA.token,
+//     });
 
-    expect(result.statusCode).toBe(200);
-    expect(result.body).toHaveProperty("updated");
-  });
-});
+//     expect(result.statusCode).toBe(200);
+//     expect(result.body).toHaveProperty("updated");
+//   });
+// });
 
-describe("DELETE /users/username", () => {
-  test("delete user", async () => {
-    const result = await request(app).delete("/users/testUserName");
-    expect(result.statusCode).toBe(200);
-    expect(result.text).toContain("User deleted");
-  });
+// describe("DELETE /users/username", () => {
+//   test("delete user", async () => {
+//     const received= await request(app).delete("/users/testUserName");
+//     expect(result.statusCode).toBe(200);
+//     expect(result.text).toContain("User deleted");
+//   });
+// });
+
+// afterEachHook 
+
+// afterAll(async function () {
+//   await afterAllHook();
+//   await db.end();
+// });
+
+afterEach(async function () {
+  try {
+    await db.query("DELETE FROM users");
+  } catch (error) {
+    console.error(error);
+  }
 });
 
 afterAll(async function () {
-  await afterAllHook();
+    try {
+      await db.end();
+    } catch (err) {
+      console.error(err);
+    }
 });
+
+
+// async function afterEachHook() {
+//   try {
+//     await db.query("DELETE FROM jobs");
+//     await db.query("DELETE FROM users");
+//     await db.query("DELETE FROM companies");
+//   } catch (error) {
+//     console.error(error);
+//   }
+// }
+
+// async function afterAllHook() {
+//   try {
+//     await db.end();
+//   } catch (err) {
+//     console.error(err);
+//   }
+// }
